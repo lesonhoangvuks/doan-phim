a6d3ec16163bd169267922cb11e58fbe42f0d80b
Hoang Vu <hoangvuoppo7@gmail.com>
2022-05-19 15:14:12 +0700

    tach header footer

---
+++ inc/footer.php
@@ -0,0 +1,78 @@
+<footer class="footer ">
+          <div class="section-wrapper">
+               <div class="row ">
+                   <div class="col-6 footer-header">
+                         <a href="#" class="logo">
+                              <i style="margin-right: 10px;" class='bx bx-movie-play bx-tada main-color'></i>Fl<span class="main-color">i</span>x
+                         </a>
+
+                         <p class="description">
+                              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                              Quas, possimus eius. Deserunt non odit, cum vero reprehenderit
+                              laudantium odio vitae autem quam, incidunt molestias ratione mollitia accusantium,
+                              facere ab suscipit.
+                         </p>
+                         <div class="social-list">
+                              <a href="#" class="social-item">
+                                   <i class="bx bxl-facebook"></i>
+                              </a>
+                              <a href="#" class="social-item">
+                                   <i class="bx bxl-instagram"></i>
+                              </a>
+                              <a href="#" class="social-item">
+                                   <i class="bx bxl-twitter"></i>
+                              </a>
+                         </div>
+                   </div>
+
+                   <div class="col-12 footer-item">
+                         <div class="row">
+                              <div class="col-4 align-items-center">
+                                   <div class="content">
+                                        <p class="main-color" style="font-size: 1.2rem;"><b>Flix</b></p>
+                                        <ul class="footer-menu">
+                                            <li><a href="#"> About us</a></li>
+                                            <li><a href="#"> My profile</a></li>
+                                            <li><a href="#"> Pricing plans</a></li>
+                                            <li><a href="#"> Contacts</a></li>
+                                        </ul>
+                                     </div>
+                              </div>
+
+
+                              <div class="col-4 align-items-center">
+                                   <div class="content">
+                                        <p class="main-color" style="font-size: 1.2rem;"><b>Browse</b></p>
+                                        <ul class="footer-menu">
+                                            <li><a href="#">Live TV</a></li>
+                                            <li><a href="#">Live Movies</a></li>
+                                            <li><a href="#">Live Series</a></li>
+                                            <li><a href="#">Streaming Library</a></li>
+                                        </ul>
+                                     </div>
+                              </div>
+
+                              <div class="col-4 align-items-center">
+                                   <div class="content">
+                                        <p class="main-color" style="font-size: 1.2rem;"><b>Help</b></p>
+                                        <ul class="footer-menu">
+                                            <li><a href="#">Account & Billing</a></li>
+                                            <li><a href="#">Plans & Pricing</a></li>
+                                            <li><a href="#">Supported devices</a></li>
+                                            <li><a href="#">Accessibility</a></li>
+                                        </ul>
+                                     </div>
+                              </div>
+
+                            
+                         </div>
+                   </div>
+               </div>
+
+
+          
+          </div>
+      </footer>
+
+
+     
